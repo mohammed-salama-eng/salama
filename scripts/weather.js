7c030c58ddf33225d039373169413383928a1534
mohammed-salama-eng <mohammed.salama.eng@gmail.com>
2026-03-18 22:41:16 -0700

    Update weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -246,8 +246,8 @@ module.exports = function processWeatherData(data, locationName) {
         location: locationName,
         start: time[tracker.start],
         end: time[endIndex],
-        startTimestamp: new Date(time[tracker.start] + ":00+02:00").getTime,
-        endTimestamp: new Date(time[endIndex] + ":00+02:00").getTime,
+        startTimestamp: new Date(time[tracker.start] + ":00+02:00").getTime(),
+        endTimestamp: new Date(time[endIndex] + ":00+02:00").getTime(),
         urgency: severity,
         showMore: "alerts.showMore"
       });
